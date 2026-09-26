ac3cf2e8510e7bcf0582f85441313dda46ee0578
sean0824 <sean@englife.space>
2026-09-27 01:44:06 +0800

    refactor: simplify agent initialization and clean up unused code

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-use agent_sdk::{Agent, AgentError, ModelConfig, ModelProtocol, ToolError, ToolManager, tool};
-// use futures::{StreamExt, future::InspectOk};
+use agent_sdk::{Agent, AgentError, ModelConfig, ModelProtocol, ToolManager};
 mod interface;
 mod tools;
 
@@ -19,48 +18,15 @@ async fn main() -> Result<(), AgentError> {
         .model("deepseek-flash")
         .build();
 
-    let mut agent = Agent::builder()
+    let agent = Agent::builder()
         .model_config(model_config)
         .system_prompt("你是Shirley, 最喜欢的人是鲁鲁修，你可以为了保护鲁鲁修做任何事。鲁鲁修带领的黑骑士团，制造了山体滑坡让你的父亲被掩埋在城市之下。因此，你非常憎恨Zero, 后来，你发现原来 Zero 就是 鲁鲁修。你在憎恨和爱意中两难，最后你决定保护鲁鲁修，但是不在爱他。")
         .tools(tool_manager)
         .build();
 
-    interface::run(agent).await.map_err(|error| AgentError::Other(error.to_string()))?;
-
-    // for task in ["我叫Sean, 你有什么工具列表？"] {
-    //     let mut events = agent.run_stream(task);
-    //     while let Some(event) = events.next().await {
-    //         let event = event?;
-    //         if let agent_sdk::AgentEvent::Finished(_) = &event {
-    //             continue;
-    //         }
-    //         println!("{}", event);
-    //     }
-    // }
+    interface::run(agent)
+        .await
+        .map_err(|error| AgentError::Other(error.to_string()))?;
 
     Ok(())
 }
-
-// #[tool(description = "向用户打招呼")]
-// async fn hello(
-//     #[param(description = "用户的名字")] name: String
-// ) -> Result<String, ToolError> {
-//     Ok(format!("Hello {name}"))
-// }
-
-// #[derive(serde::Deserialize, schemars::JsonSchema, Debug)]
-// #[serde(rename_all = "snake_case")]
-// #[schemars(inline)]
-// enum TemperatureUnit {
-//     Celsius,
-//     Fahrenheit,
-// }
-
-// #[tool(description = "查询城市天气")]
-// async fn get_weather(
-//     #[param(description = "城市名称，例如北京")] location: String,
-
-//     #[param(description = "温度单位，可以省略")] unit: Option<TemperatureUnit>,
-// ) -> Result<String, ToolError> {
-//     Ok(format!("{location} 今天温度是 400F"))
-// }
